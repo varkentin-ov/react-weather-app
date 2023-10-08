1f2df4b61d6022a5258c048c3be65e1d2106b33d
Olena <varkentin.elena@gmail.com>
2023-10-08 12:58:36 +0300

    added bootstrap

--- src/App.1.js
+++ src/App.1.js
@@ -1,17 +1,23 @@
+import React from "react";
+import "./App.css";
+
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Olena Varkentin and is{" "}
-        <a
-          href="https://github.com/varkentin-ov/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project is created by Olena Varkentin and is{" "}
+          <a
+            href="https://github.com/varkentin-ov/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
