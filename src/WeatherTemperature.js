916dc1e35a1ae37766e9f8b739ed35a86b44b768
Olena <varkentin.elena@gmail.com>
2023-10-10 15:36:41 +0300

    Added unit component

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+export default function WeatherTempUnits(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function convertToImperial(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToMetric(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  if (unit === "celsius") {
+    return (
+      <span>
+        <span class="temperature">{Math.round(props.celsius)}</span>
+        <span class="unit">
+          °C |{" "}
+          <a href="/" onClick={convertToImperial}>
+            °F
+          </a>{" "}
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span>
+        <span class="temperature">{Math.round(fahrenheit())}</span>
+        <span class="unit">
+          <a href="/" onClick={convertToMetric}>
+            °C
+          </a>{" "}
+          | °F{" "}
+        </span>
+      </span>
+    );
+  }
 }
